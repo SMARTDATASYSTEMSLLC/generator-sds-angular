f3723e6912147b8853d855bccfeb7cc4e591255b
David Benson <davidbenson86@gmail.com>
2016-06-20 10:53:16 -0400

    fixing component jslint error

--- component/templates/complex/directive.js
+++ component/templates/complex/directive.js
@@ -1,15 +1,15 @@
 (function () {
     'use strict';
-    function <%= lodash.camelCase(name) %>Controller () {
-
+    function <%= lodash.capitalize(lodash.camelCase(name)) %>Controller () {
+        var $ctrl = this;
     }
 
     angular.module('<%= appname %>').component('<%= lodash.camelCase(name) %>', {
-          templateUrl: '<%= htmlPath %>',
-          controller: <%= lodash.camelCase(name) %>Controller,
-          bindings: {
-           
-          }
+        templateUrl: '<%= htmlPath %>',
+        controller: <%= lodash.capitalize(lodash.camelCase(name)) %>Controller,
+        bindings: {
+
+        }
     });
 
 })();
